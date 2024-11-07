969ad5b32290f70637ae86745cfecd5fe00e8dd9
VoltagedDebunked <rusindanilo@gmail.com>
2024-11-07 21:06:29 +0200

    Optimized IRQ to save CPU cycles

--- src/interrupt/irq.c
+++ src/interrupt/irq.c
@@ -1,20 +1,8 @@
 #include "irq.h"
 
-void irq_handler(void)
-{
-    unsigned char dummy;
-
+void irq_handler(void) {
     // Send EOI to slave PIC first (IRQ8-15)
-    __asm__ volatile("outb %%al, $0xA0" : : "a"(0x20));
-
+    outb(0x20, 0xA0);
     // Send EOI to master PIC (IRQ0-7)
-    __asm__ volatile("outb %%al, $0x20" : : "a"(0x20));
-
-    // Read from master PIC to ensure interrupt was acknowledged
-    __asm__ volatile("inb $0x20, %%al" : "=a"(dummy));
-
-    // Read from slave PIC to ensure interrupt was acknowledged
-    __asm__ volatile("inb $0xA0, %%al" : "=a"(dummy));
-
-    (void)dummy; // To avoid unused variable warning, i hate those so much...
+    outb(0x20, 0x20);
 }
